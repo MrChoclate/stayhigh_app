d5a0120f7fee3b108ec869293a760457b38f4371
choclate <villaincle@eisti.eu>
2015-06-28 22:19:41 +0200

    Fix error if cordova is undefined (web)

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -6,14 +6,17 @@ app.controller('BarcodeCtrl', ['$scope', '$cordovaBarcodeScanner',
     function ($scope, $cordovaBarcodeScanner) {
 
         $scope.scanBarcode = function () {
-            console.log('test');
-            $cordovaBarcodeScanner.scan().then(function (imageData) {
-                console.log(imageData.text);
-                console.log('Barcode Format -> ' + imageData.format);
-                console.log('Cancelled -> ' + imageData.cancelled);
-            }, function (error) {
-                console.log('An error happened -> ' + error);
-            });
+            if (typeof cordova !== 'undefined') {
+                $cordovaBarcodeScanner.scan().then(function (imageData) {
+                    console.log(imageData.text);
+                    console.log('Barcode Format -> ' + imageData.format);
+                    console.log('Cancelled -> ' + imageData.cancelled);
+                }, function (error) {
+                    console.log('An error happened -> ' + error);
+                });
+            } else {
+                console.log('Cordova is undefined');
+            }
         };
     }
 ]);
